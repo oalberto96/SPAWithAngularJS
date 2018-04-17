73239eddd48300b9a3afaaa96a8f9ab7f2ac8f24
oalberto96 <ochoapublic@gmail.com>
2018-04-16 21:14:34 -0700

    Lunch Check done

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -1,1 +1,24 @@
-var app = angular.module("LunchCheck",[]);
+(function () {
+	"use-strict";
+	var app = angular.module("LunchCheck",[]);
+	app.controller("LunchCheckController", LunchCheckController);
+
+	LunchCheckController.$inject = ['$scope'];
+	function LunchCheckController($scope){
+
+		$scope.lunchCheck = function(){
+			if ($scope.lunchList != undefined && $scope.lunchList != "") {
+				var lunchList = $scope.lunchList.split(",");
+				if (lunchList.length <= 3) {
+				$scope.message = "Enjoy!";
+				} else {
+					$scope.message = "Too much!";
+				}
+			} else {
+				$scope.message = "Please enter data first";
+			}
+		}
+	}
+
+})();
+
