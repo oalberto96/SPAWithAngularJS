b4455453a3bdbd3f46529f3ec864ab5b778a2739
oalberto96 <ochoapublic@gmail.com>
2018-04-16 22:20:58 -0700

    Add bonus

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -9,13 +9,24 @@
 		$scope.lunchCheck = function(){
 			if ($scope.lunchList != undefined && $scope.lunchList != "") {
 				var lunchList = $scope.lunchList.split(",");
+				checkEmptyItems(lunchList);
 				if (lunchList.length <= 3) {
 				$scope.message = "Enjoy!";
 				} else {
 					$scope.message = "Too much!";
+					$scope.typeMessage = "success";
 				}
 			} else {
+				$scope.typeMessage = "error";
 				$scope.message = "Please enter data first";
+			}
+		}
+
+		function checkEmptyItems(list){
+			for (var i = 0; i < list.length; i++) {
+				if (list[i] == "") {
+					list.splice(i, 1);
+				}
 			}
 		}
 	}
